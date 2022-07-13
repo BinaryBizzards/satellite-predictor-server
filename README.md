4263e07139d6b5c3eec64aa8654dcc586bc16ce0
anisha-01-phoenix <anisha.dat01@gmail.com>
2022-07-14 01:49:07 +0530

    Authentication added

--- README.md
+++ README.md
@@ -1,13 +1,93 @@
-# Satellite Image Terrain Predictor Server
-### Flask API to predict the terrain type of the satellite images.
+# __Satellite Image Terrain Predictor Server__
+## Flask API to predict the terrain type of the satellite images.
 
-* Language: Python
-* Database: SQLAlchemy
+* Language : `Python`
+* Database : `SQLAlchemy` (*satellite_predictor.db*)
 
-> GET /get_list
->> Gives the list of all the images which have been predicted
+## API Documentation :
 
-> POST/predict
+### __Authentication__
+
+#### (Using Flask Login)
+
+> POST `/signup`
+>> REQUEST :
+>>> name : String
+
+>>> email : String
+
+>>> password : String
+
+>> RESPONSE :
+>>> JSON Message indicating whether User is registered or not.
+
+> POST `/login`
+>> REQUEST :
+>>> email : String
+
+>>> password : String
+
+>>> remember : Boolean
+
+>> RESPONSE :
+>>> JSON Message indicating whether User is logged in or not.
+
+> GET `/logout`
+>>__@login_required__
+
+>> RESPONSE :
+>>> JSON Message indicating whether User is logged out or not.
+
+### Satellite List
+
+> GET `/get_list`
+>>__@login_required__
+>> Gives the list of all the images which have been predicted by the current User (History)
+
+> POST `/predict`
+>>__@login_required__
 >> Takes the satellite image as request and gives the predicted results as response.
 
-[Training Dataset](https://www.kaggle.com/datasets/nikitarom/planets-dataset/code?select=planet)
+
+## Database Tables
+
+```SQL
+__tablename__ = 'satellite'
+satellite_id = Column(Integer, primary_key=True)
+img = Column(String)
+predicted_values = Column(String)
+user_id = Column(Integer)
+```
+
+```SQL
+ __tablename__ = 'users'
+id = Column(Integer, primary_key=True) 
+name = Column(String)
+email = Column(String, unique=True)
+password = Column(String)
+```
+
+## Data Training and Prediction Model
+
+* [Training Dataset Link](https://www.kaggle.com/datasets/nikitarom/planets-dataset/code?select=planet)
+
+* [Dataset Model](/resnet_model.pth)
+
+## Libraries Used
+```r
+albumentations==1.1.0
+Flask==2.1.2
+flask-marshmallow==0.14.0
+Flask-SQLAlchemy==2.5.1
+Flask-Login==0.6.1
+numpy==1.23.0
+opencv-python-headless==4.6.0.66
+pandas==1.4.3
+sklearn==0.0
+SQLAlchemy==1.4.39
+SQLAlchemy-Utils==0.38.2
+timm==0.5.4
+torch==1.11.0
+torchvision==0.12.0
+gunicorn==20.1.0
+```
--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import base64
 from dataclasses import fields
 import os
-from flask import Flask, redirect, url_for, request, jsonify
+import re
+from flask import Flask, request, jsonify
 from flask_marshmallow import Marshmallow
+from flask_login import login_user, login_required, logout_user, current_user
+from flask_login import UserMixin, LoginManager
+# from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from sklearn.preprocessing import MultiLabelBinarizer
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import Column, Integer, String
@@ -15,15 +19,21 @@ import timm
 import torch.nn as nn
 import albumentations as A
 from albumentations.pytorch.transforms import ToTensorV2
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
-    os.path.join(basedir, 'satellite.db')
+    os.path.join(basedir, 'satellite_predictor.db')
+
+app.config['SECRET_KEY'] = 'Satelitain@6877'
+app.config['SESSION_TYPE'] = 'filesystem'
+
 MODEL_PATH = 'resnet_model.pth'
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
+lm = LoginManager(app)
 
 # create database
 def db_create():
@@ -31,25 +41,43 @@ def db_create():
     print('Database Created!')
 
 #insert data
-def db_seed(img_encode,prediction):
-    satellite = Satellite(img=img_encode,predicted_values=prediction)
+def db_seed(img_encode,prediction,user_id):
+    satellite = Satellite(img=img_encode,predicted_values=prediction,user_id=user_id)
     db.session.add(satellite)
     db.session.commit()
     print('Database seeded!')
 
 class Satellite(db.Model):
-    _tablename = 'satellite'
-    id = Column(Integer, primary_key=True)
+    __tablename__ = 'satellite'
+    satellite_id = Column(Integer, primary_key=True)
     img = Column(String)
     predicted_values = Column(String)
+    user_id = Column(Integer)
 
 
 class SatelliteSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'img', 'predicted_values')
+        fields = ('satellite_id', 'img', 'predicted_values','user_id')
 
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True) 
+    name = Column(String)
+    email = Column(String, unique=True)
+    password = Column(String)
 
+class UserSchema(ma.Schema) :
+    class Meta:
+        fields=('id' , 'name' , 'email' , 'password')
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
 satellite_schema = SatelliteSchema(many=True)
+
+@lm.user_loader
+def load_user(id) :
+    return User.query.get(int(id))
+
 
 CFG = {
     "img_size": 224,
@@ -97,12 +125,10 @@ model = ResNet18(CFG["model_name"], CFG["num_classes"])
 model.to(CFG["device"])
 print(model.parameters)
 
-# model = models.resnet18(pretrained=True)
 model.load_state_dict(torch.load(MODEL_PATH, map_location=torch.device('cpu')))
 
 thresh = [0.21, 0.07, 0.11, 0.19, 0.05, 0.16, 0.08, 0.03,
           0.13, 0.19, 0.17, 0.11, 0.27, 0.19, 0.18, 0.1, 0.18]
-
 
 def pred_single(img_encode, return_label=True):
     with torch.no_grad():
@@ -121,12 +147,70 @@ def pred_single(img_encode, return_label=True):
         # tags = " ".join(yhat_test_inv)
         return yhat_test_inv
 
+def validate(email):
+    regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+    if(re.fullmatch(regex, email)):
+        return True
+    return False
 
 @app.route('/', methods=['GET'])
-def index():
-    return jsonify("ML")
+def index():    
+    return jsonify("Satellitain")
+
+@app.route('/login', methods=['POST'])
+def login():
+    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+        db_create()
+    email = request.form['email']
+    password = request.form['password']
+    remember = True if request.form['remember'] else False
+    if not email or not password :
+        return jsonify("Please fill all the fields"), 400
+    user = User.query.filter_by(email=email).first()
+    if not user :
+        return jsonify("User is not registered yet"), 404
+    if not check_password_hash(user.password, password) :
+        return jsonify("Incorrect Password"), 400
+    login_user(user, remember=remember)
+    return jsonify("User Logged in Successfully")
+    
+
+@app.route('/signup', methods=['POST'])
+def signup():
+    if not database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
+        db_create()
+    email = request.form['email']
+    name = request.form['name']
+    password = request.form['password']
+    if not email or not password or not name:
+        return jsonify("Please fill all the fields"), 400
+    if not validate(email) :
+        return jsonify("Enter a Valid Email ID"), 400
+    if len(password)<6 :
+        return jsonify("Password must have minimum 6 characters"), 400
+    if len(name)<3 :
+        return jsonify("Enter a Valid Name"), 400 
+    user = User.query.filter_by(email=email).first()
+    if (user) :
+        return jsonify("User already registered"), 409
+    new_user = User(email=email, name=name, password=generate_password_hash(password, method='sha256'))
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify("User registered Successfully")
+
+@app.route('/logout', methods=['GET','POST'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify("Account signed out")
+
+@app.route('/profile', methods=['GET'])
+@login_required
+def profile() :
+    return jsonify(name=current_user.name, email=current_user.email)
 
 @app.route('/predict', methods=['GET', 'POST'])
+@login_required
 def upload():
     if request.method == 'POST':
         # Get the file from post request
@@ -140,20 +224,23 @@ def upload():
             s = s+str(x)+","
         s = s[:-2]
         s = s[1:]
+        img_encode=img_encode[:-1]
+        img_encode=img_encode[2:]
         if database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-            db_seed(str(img_encode),s)
+            db_seed(str(img_encode),s,current_user.id)
         else :
             db_create()
-            db_seed(str(img_encode),s)
+            db_seed(str(img_encode),s,current_user.id)
         list = s.split(',')
-        return jsonify(prediction=list)
+        return jsonify(prediction=list, user=current_user.id)
     return None
 
 @app.route('/get_list', methods=['GET'])
+@login_required
 def get_list():
     # get all the data from database
     if database_exists(app.config["SQLALCHEMY_DATABASE_URI"]):
-        satellite_list = Satellite.query.all()
+        satellite_list = Satellite.query.filter_by(user_id=current_user.id)
         result = satellite_schema.dump(satellite_list)
         return jsonify(result),200
     else :
